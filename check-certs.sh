225bc43443e691967af94829247a410dcce50f98
prime404 <22512746+Prime404@users.noreply.github.com>
2021-06-22 11:53:59 +0200

    Added initial support for file based lookups

--- check-certs.sh
+++ check-certs.sh
@@ -19,33 +19,38 @@ function send_webhook {
 function send_smtp {
   # Generate message
   read -r -d '' mail_content <<- EOM
-	Hello,
-	This message serves as a notice for that the certificate for $TARGET, expires in less than $DAYS days, on $(date -d @$expirationdate '+%Y-%m-%d').
+  Hello,
+  This message serves as a notice for that the certificate for $TARGET, expires in less than $DAYS days, on $(date -d @$expirationdate '+%Y-%m-%d').
 
-	These e-mails will be sent out till the certificate is renewed or removed from the datasource.
+  These e-mails will be sent out till the certificate is renewed or removed from the datasource.
 
-	Yours truly,
-	ssl_alert.sh on behalf of $(whoami)@$(hostname)
-	EOM
+  Yours truly,
+  ssl_alert.sh on behalf of $(whoami)@$(hostname)
+  EOM
 
   # Send e-mail alert using above SMTP details
   sendEmail -t "$EMAIL_RECIPIENT" -u "Certificate expires soon for $TARGET" -f "$SMTP_USER" -s "$SMTP_HOST" -xu "$SMTP_USER" -xp "$SMTP_PASS" -m "$mail_content" > /dev/null
 }
 
-# Check if database and table exists before proceeding
-if ! mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" -e "use $MYSQL_DB;" ; then
-  echo "Error: Database does not exist or wrong connection details." >> /dev/stderr
-  exit 2; # Exit script to not cause further errors
-elif ! mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" "$MYSQL_DB" -e "SELECT * from \`${MYSQL_TABLE}\`;" >/dev/null ; then
-  echo "Error: Table does not exist or wrong connection details specificed." >> /dev/stderr
-  exit 3; # Exit script to not cause further errors
+
+if [ "$DATASOURCE" == "MYSQL" ] ; then
+  # Check if database and table exists before proceeding
+  if ! mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" -e "use $MYSQL_DB;" ; then
+    echo "Error: Database does not exist or wrong connection details." >> /dev/stderr
+    exit 2; # Exit script to not cause further errors
+  elif ! mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" "$MYSQL_DB" -e "SELECT * from \`${MYSQL_TABLE}\`;" >/dev/null ; then
+    echo "Error: Table does not exist or wrong connection details specificed." >> /dev/stderr
+    exit 3; # Exit script to not cause further errors
+  fi
+
+  # Fetch data from database and store it for further use
+  domains="$(mysql -N -B -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" "$MYSQL_DB" -e "SELECT \`${MYSQL_COL}\` from \`${MYSQL_TABLE}\`;" | sort -u | sed 's/\*.//')"
+elif [ "$DATASOURCE" == "LOCAL_TXT" ] && [ -f "$TXT_SOURCE" ] ; then
+  domains="$(cat $TXT_SOURCE)"
 fi
 
-# Fetch data from database and store it for further use
-domains_from_db="$(mysql -N -B -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASS" "$MYSQL_DB" -e "SELECT \`${MYSQL_COL}\` from \`${MYSQL_TABLE}\`;" | sort -u | sed 's/\*.//')"
-
 # Send an e-mail for every domain that expires within 14 days
-for TARGET in $domains_from_db ; do
+for TARGET in $domains ; do
   echo $TARGET
   if [ "$(nc -z -w2 $TARGET 443 >& /dev/null; echo $?)" == "0" ] ; then
     DAYS=14;
@@ -58,7 +63,6 @@ for TARGET in $domains_from_db ; do
     if [ $in7days -gt $expirationdate ] ; then
         echo "KO - Certificate for $TARGET expires in less than $DAYS days, on $(date -d @$expirationdate '+%Y-%m-%d')"
         if [ "$WARNING_METHOD" == "WEBHOOK" ] ; then
-          echo Sending webhook
           send_webhook
         elif [ "$WARNING_METHOD" == "SMTP" ] ; then
           send_smtp
